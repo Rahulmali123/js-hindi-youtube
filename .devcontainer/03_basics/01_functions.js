1fd9a3b1143cd98a92e12d4ce5514b2340733a51
Rahulmali123 <98895527+Rahulmali123@users.noreply.github.com>
2025-08-08 11:03:15 +0000

    changes

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -8,23 +8,25 @@ function sayMyName(){
     console.log("H");
 }
 
-// sayMyName()
+ sayMyName()
 
-// function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2){
 
-//     console.log(number1 + number2);
-// }
+    console.log(number1 + number2);
+}
+
+// addTwoNumbers(3,4);
 
 function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
-    // return result
+    // return result;
     return number1 + number2
 }
 
 const result = addTwoNumbers(3, 5)
 
-// console.log("Result: ", result);
+console.log("Result: ", result);
 
 
 function loginUserMessage(username = "sam"){
@@ -35,15 +37,15 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
+console.log(loginUserMessage("sams"))
 // console.log(loginUserMessage("hitesh"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(val1,val2,val3, ...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+console.log(calculateCartPrice(200, 400, 500, 2000))
 
 const user = {
     username: "hitesh",
@@ -55,6 +57,7 @@ function handleObject(anyobject){
 }
 
 // handleObject(user)
+
 handleObject({
     username: "sam",
     price: 399
@@ -66,5 +69,5 @@ function returnSecondValue(getArray){
     return getArray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
+ console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,62 @@
+// var c = 300
+ let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    var c=30
+    // console.log("INNER: ", a);
+    
+}
+
+//  console.log(a);
+// //  console.log(b);
+// console.log(c);
+
+
+// function one(){
+//     const username = "hitesh"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     //  console.log(website);
+
+//      two()
+
+// }
+//  one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// // ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+
+
+function addone(num){
+    return num + 1
+}
+
+ console.log(addone(5))
+
+
+
+
+const addTwo = function(num1){
+    return num1 + 2
+}
+
+ addTwo(5)
+
+ console.log(addTwo(5))
